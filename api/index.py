5c6ef7255df35ee4fe017510dc1fc9604b4174a0
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2024-03-17 11:54:39 +0100

    Changes

--- api/index.py
+++ api/index.py
@@ -1,21 +1,16 @@
 from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-  return "<h1 style='text-align: center;'>Aircall Webhook Server is Running</h1>"
 
 @app.route("/aircall/webhook", methods=["POST"])
 def handle_webhook():
-  # Handle the webhook payload from Aircall
-  data = request.json
-  print(data)  # For demonstration, print the data to console
+    data = request.json
+    print(data)  # For demonstration purposes
+    # Process the data asynchronously here
+    return jsonify(success=True), 200
 
-  # Process the data asynchronously here (e.g., queue a background job)
-
-  # Always return a 200 HTTP status code to Aircall
-  return jsonify(success=True), 200
 
 if __name__ == "__main__":
-  app.run(port=5000)
+    app.run(port=500)
